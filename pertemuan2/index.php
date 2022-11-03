ff73ff30ade250856a49ca79e215acc8abeea592
unknown <saidahraudatul1303@gmail.com>
2022-11-03 10:35:25 +0800

    kuliah hari ini 03/11/2022

---
+++ pertemuan2/index.php
@@ -0,0 +1,29 @@
+<?php 
+var_dump("hello world!");
+
+$nama = "saidah raudatul jannah";
+echo "hallo, nama saya $nama";
+
+//operator
+//aritmatika
+//*/+ - %
+//$x = 10;
+//$y = 20;
+//echo $x + $y;
+
+//penggabungan sting/ concatenation/ concat
+// .
+// $nama_depan = "saidah";
+// $nama_belakang = "raudatuljannah";
+// echo $nama_depan . " " . $nama_belakang; 
+
+//assignment
+// =, +=, -=, *=, /=, %=, .=
+$x = 1;
+$x += 5;
+echo $x;
+$nama .= $nama_depan;
+$nama .= " ";
+$nama .= $nama_belakang;
+echo $nama;
+?>
