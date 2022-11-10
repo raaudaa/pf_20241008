147e9033f2596c62a4b63ea87f124a01f7990c6e
unknown <saidahraudatul1303@gmail.com>
2022-11-10 10:38:08 +0800

    kuliah/10/11/2022

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,23 @@
+<?php 
+// perulangan looping
+// for, while, do.. while
+// foreach : pengulangan khusus array
+
+// menggunakan for
+// for($i = 0; $i < 5; $i++){
+// echo "Hello World! <br>";
+// }
+
+// menggunakan while
+// $i = 0;
+// while($i < 5){
+//     echo "Hello World! <br>";
+// $i++;
+// }
+
+$i=0;
+while($i < 5){
+    echo"rauda <br>";
+    $i++;
+}
+ ?>
---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,22 @@
+<?php 
+// kondisi atau percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+// if
+// $x = 10;
+// if( $x < 20 ){
+//     echo "benar";
+// }
+// echo "salah";
+
+// if else
+// $x = 10;
+// if( $x <20){
+//     echo "benar";
+// }else{
+//     echo "salah";
+// }
+?>
