41538da632b2d08e42d108d45c66dd2c92691e92
unknown <saidahraudatul1303@gmail.com>
2022-12-08 10:31:13 +0800

    kuliah hari ini 08/12/2022

---
+++ login/admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>admin page</title>
+</head>
+<body>
+    
+<h1>selamat datang, admin</h1>
+
+<a href="login.php">logout</a>
+</body>
+</html>
---
+++ login/login.php
@@ -0,0 +1,48 @@
+<?php 
+// cek tombol submit sudah ditekan atau belum
+if( isset($_POST["submit"])){
+
+    //cek username & pasword
+    if($_POST["username"]=="admin" && $_POST["password"]=="123"){
+
+        //jika benar, redirect ke halaman admin
+        header("Location: admin.php");
+        exit;
+
+    } else {
+        //jika salah, tampilkan pesan kesalahan
+        $error= true;
+    }
+}
+?>
+ 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>login</title>
+</head>
+<body>
+    
+<h1>halaman login</h1>
+
+<?php if (isset($error)): ?>
+<p style="color:red; font-style:italic;">username/pasword salah!</p>
+<?php endif; ?>
+
+<form action=""method="post">
+    <li>
+        <label for="username">username :</label>
+        <input type="text"name= "username" id="username">
+    </li>
+    <li>
+        <label for="pasword">pasword :</label>
+        <input type="password"name= "password" id="password">
+    </li> 
+    <li>
+        <button type="submit" name="submit">login</button>
+    </li>
+</form>
+</body>
+</html>
